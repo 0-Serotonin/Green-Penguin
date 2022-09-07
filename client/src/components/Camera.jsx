a32ea876af0a55ea41fb6c1f8a4a304957039c17
jeslynlamxy <jeslynlxy@hotmail.com>
2022-09-08 02:52:40 +0800

    wordings

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -2,9 +2,26 @@ import React from "react";
 
 function Camera() {
     return (
-        <div>
-            <h1>This is the camera module hehe</h1>
-        </div>
+        <div class="container">
+      <div style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '20vh'}}>
+      <h1 class="font-weight-light">Item Classification</h1>
+      </div>
+
+      <div class ="row">
+
+      <div class="col-lg-12">
+      <h3 style ={{fontFamily:'Playfield display',fontWeight:'bold', height:'10vh'}}> Find out where to recycle your items  qwery</h3>
+      </div>
+
+      <div class="col-lg-2">
+        <Button onClick = {null}>UPLOAD IMAGE</Button>
+      </div>
+      <div class="col-lg-4">
+        <Button onClick = {null}>USE CAMERA</Button>
+      </div>
+      </div>
+      </div>
+      
     )
 }
 
