88fe87655453bd888acb8466970d441484f55f26
jeslynlamxy <jeslynlxy@hotmail.com>
2022-09-08 02:58:38 +0800

    fancy

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -3,26 +3,22 @@ import styled from "styled-components";
 
 function Camera() {
     return (
-        <div class="container">
+    <div class="container">
       <div style={{display: 'flex', justifyContent:'center', alignItems:'center', height: '20vh'}}>
-      <h1 class="font-weight-light">Item Classification</h1>
+        <h1 class="font-weight-light">Item Classification</h1>
       </div>
-
       <div class ="row">
-
-      <div class="col-lg-12">
-      <h3 style ={{fontFamily:'Playfield display',fontWeight:'bold', height:'10vh'}}>Find out where to recycle your items</h3>
+        <div class="col-lg-8">
+            <h3 style ={{fontFamily:'Playfield display',fontWeight:'bold', height:'10vh'}}>Find out where to recycle your items</h3>
+        </div>
+        <div class="col-lg-2">
+            <Button onClick = {null}>UPLOAD IMAGE</Button>
+        </div>
+        <div class="col-lg-2">
+            <Button onClick = {null}>USE CAMERA</Button>
+        </div>
       </div>
-
-      <div class="col-lg-2">
-        <Button onClick = {null}>UPLOAD IMAGE</Button>
-      </div>
-      <div class="col-lg-4">
-        <Button onClick = {null}>USE CAMERA</Button>
-      </div>
-      </div>
-      </div>
-
+    </div>
     )
 }
 const Button = styled.button`
