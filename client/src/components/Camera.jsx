d38fa8d4d59701e8119cebd8f4e540ee2e8a1b00
jeslynlamxy <jeslynlxy@hotmail.com>
2022-09-08 02:52:08 +0800

    hehe

--- client/src/components/Camera.jsx
+++ client/src/components/Camera.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Camera() {
     return (
         <div>
-            <h1>This is the camera module</h1>
+            <h1>This is the camera module hehe</h1>
         </div>
     )
 }
